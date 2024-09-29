5a974abf67b94a3a4ca3e4a1cbd2f8c39b24cda5
rdoshiro <167146744+rdoshiro@users.noreply.github.com>
2024-09-28 19:52:17 -0700

    Project1_AI.py

--- Project1_AI.py
+++ Project1_AI.py
@@ -5,7 +5,7 @@ import numpy as np
 import google.generativeai as genai
 from PIL import Image
 
-api_key = st.secrets ["AIzaSyBa0nA2SbuamCQPsUB93BgtJzcTKuRTYDI"]
+api_key = st.secrets ["GOOGLE_API_KEY"]
 
 genai.configure(api_key = api_key )
 model = genai.GenerativeModel("gemini-1.5-flash")
